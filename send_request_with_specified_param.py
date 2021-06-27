d776554bab4d5564d9584e90fda628da5afa659b
QuadBitBrain <quadbitbrain@gmail.com>
2021-06-28 00:26:02 +0400

    Simplified nested loops, fixed wrong enum calls in custom method function

--- send_request_with_specified_param.py
+++ send_request_with_specified_param.py
@@ -2,18 +2,20 @@ import requests
 import RequestMethods
 
 methods = RequestMethods.MethodsShort
+responses = []
 
 for method in methods:
-    responses = [requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type",
-                               data={"method": {method.value}}),
-                 requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type",
-                              params={"method": {method.GET.value}}),
-                 requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type",
-                              data={"method": {method.PUT.value}}),
-                 requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type",
-                                 data={"method": {method.DELETE.value}})]
 
-    for response in responses:
-        if response.request.method != method.value and response.status_code == 200 and response.reason == "OK"\
-                and response.text == '{"success":"!"}':
-            print(f"Request method was: {response.request.method}, and param was: {method.value}")
+    responses.append(requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type",
+                                   data={"method": {method.value}}))
+    responses.append(requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type",
+                                  params={"method": {method.value}}))
+    responses.append(requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type",
+                                  data={"method": {method.value}}))
+    responses.append(requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type",
+                                     data={"method": {method.value}}))
+
+for response in responses:
+    if response.request.method != method.value and response.status_code == 200 and response.reason == "OK"\
+            and response.text == '{"success":"!"}':
+        print(f"Request method was: {response.request.method}, and param was: {method.value}")
