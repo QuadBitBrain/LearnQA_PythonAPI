e216bcd6282137bce27f1a83f8ef9e6b42b2bd36
QuadBitBrain <quadbitbrain@gmail.com>
2021-06-27 23:09:40 +0400

    CRUD requests with a specified method param

---
+++ send_request_with_custom_method.py
@@ -0,0 +1,17 @@
+import requests
+import RequestMethods
+
+methods = RequestMethods.Methods
+responses = [requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type",
+                           params={"method": {methods.POST.value}}),
+             requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type",
+                           params={"method": {methods.GET.value}}),
+             requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type",
+                           params={"method": {methods.PUT.value}}),
+             requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type",
+                           params={"method": {methods.DELETE.value}})]
+
+for response in responses:
+    print(response.status_code)
+    print(response.reason)
+    print(response.text)
